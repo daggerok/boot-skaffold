ef2bc6b99ba829b57e0050590e67008ba0a19f5d
Maksim Kostromin <daggerok@gmail.com>
2019-11-07 07:47:09 +0200

    It's secret! (C) whatthecommit.com

--- src/main/kotlin/com/github/daggerok/skaffold/boot/BootSkaffoldApplication.kt
+++ src/main/kotlin/com/github/daggerok/skaffold/boot/BootSkaffoldApplication.kt
@@ -1,16 +1,24 @@
 package com.github.daggerok.skaffold.boot
 
+import org.apache.logging.log4j.LogManager
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.core.env.Environment
+import org.springframework.core.env.get
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+import java.time.LocalDateTime
 
 @RestController
-class IndexPage {
+class IndexPage(private val env: Environment) {
+
+    private val log = LogManager.getLogger()
 
     @GetMapping
-    fun index() = mapOf("nello" to "skaffold")
+    fun index() = Mono.just(mapOf("nello" to "skaffold"))
+            .doOnEach { log.info("{}: I'm in at {}!", env["HOSTNAME"] ?: "Unknown", LocalDateTime.now()) }
 }
 
 @SpringBootApplication
